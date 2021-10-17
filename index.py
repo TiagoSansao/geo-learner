d2dcf38c35f12ff74446dc54fedf18bdafb620a7
Tiago Sansão <tiagossansao@gmail.com>
2021-10-17 14:51:24 -0300

    Added static typing to all variables

--- index.py
+++ index.py
@@ -1,13 +1,14 @@
 import json
 import random
 import os
+from typing import List, Dict
 from controllers.colors import colors
 
 mode: int
 africa, america, europa, asia, oceania = [], [], [], [], []
 
 with open("data.json", "r", encoding="utf") as f:
-    data = json.load(f)
+    data: List[Dict[str, str]] = json.load(f)
 
 for country in data:
     if country["continent"] == "Ásia":
@@ -21,7 +22,7 @@ for country in data:
     elif country["continent"] == "África":
         africa.append(country)
 
-modesData: dict = {
+modesData: Dict[str, dict] = {
     "1": data,
     "2": america,
     "3": europa,
@@ -33,6 +34,7 @@ modesData: dict = {
 
 def start():
     os.system("cls|clear")
+
     print(f"{colors.Cyan}Em qual continente você deseja jogar?")
     print(f"{colors.Magenta}[1]{colors.Blue} Todos")
     print(f"{colors.Magenta}[2]{colors.Blue} América")
@@ -40,32 +42,34 @@ def start():
     print(f"{colors.Magenta}[4]{colors.Blue} África")
     print(f"{colors.Magenta}[5]{colors.Blue} Ásia")
     print(f"{colors.Magenta}[6]{colors.Blue} Oceania")
+
     try:
-        mode = int(input(f"{colors.Yellow}Resposta: {colors.White}"))
+        mode: int = int(input(f"{colors.Yellow}Resposta: {colors.White}"))
         os.system("cls|clear")
         if mode not in range(1, 7):
             return start()
     except:
         return start()
+
     startMode(mode)
 
 
 def startMode(mode: int):
     for modeStr in modesData:
         if str(mode) == modeStr:
-            country = random.randrange(0, len(modesData[modeStr]) - 1)
-            countryData = modesData[modeStr][country]
+            country: int = random.randrange(0, len(modesData[modeStr]) - 1)
+            countryData: Dict[str, str] = modesData[modeStr][country]
 
     os.system("cls|clear")
 
-    print("{d}Qual é a capital do país {b}{c}{country}{rmb}{d} situado na {continent}?".format(
-        continent=countryData["continent"], country=countryData["name"], c=colors.LightCyan, d=colors.Cyan, b=colors.Bold, rmb=colors.ResetAll)
+    print(
+        f"{colors.Cyan}Qual é a capital do país {colors.Bold}{colors.LightCyan}{countryData['name']}{colors.ResetAll}{colors.Bold} situado na {countryData['continent']}?"
     )
 
-    answer = str(input(f"{colors.Yellow}Sua resposta: {colors.White}"))
-    isCorrect = str.lower(answer).strip() == str.lower(
+    answer: str = str(input(f"{colors.Yellow}Sua resposta: {colors.White}"))
+    isCorrect: bool = str.lower(answer).strip() == str.lower(
         countryData["capital"]).strip()
-    state = colors.Green + \
+    state: str = colors.Green + \
         "[Correto] " if isCorrect else colors.Red + "[Errado] "
 
     print("\n" + state + colors.Green +
@@ -73,9 +77,10 @@ def startMode(mode: int):
     print(f"{colors.Magenta}[C]{colors.Blue}      para trocar de modo")
     print(f"{colors.Magenta}[X]{colors.Blue}      para sair")
     print(
-        f"{colors.Magenta}[ENTER]{colors.Blue}  para continuar no modo")
+        f"{colors.Magenta}[ENTER]{colors.Blue}  para continuar no modo"
+    )
 
-    willContinue = str.lower(str(input()))
+    willContinue: str = str.lower(str(input()))
 
     if willContinue == "c":
         start()
